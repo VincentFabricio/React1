c5c7928557a8b0549a3c8f5c8292b36f0866ad5a
VincentFabricio <88489423+VincentFabricio@users.noreply.github.com>
2023-12-19 22:20:51 -0500

    Initial commit

---
+++ hola_mundo/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export function App () {
+    return (
+        <article >
+            <header>
+                <img alt="El avatar" src="/src/Img/Vincent_Picture.jpeg"></img>
+                <div>
+                    <strong>Vincent Fabricio</strong>
+                    <span>@vincentmh</span>
+                </div>
+                
+            </header>
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+    )
+}
